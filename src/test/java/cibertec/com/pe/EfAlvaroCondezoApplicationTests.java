86f3112574f90f8af4b77db01481ccb5d71fbab2
errornoah <54185766+errornoah@users.noreply.github.com>
2024-03-10 21:56:11 -0500

    Add files via upload

---
+++ src/test/java/cibertec/com/pe/EfAlvaroCondezoApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.com.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfAlvaroCondezoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
